80c77e63a959405f27c872ef13b8ca6c1fb77222
grotatori <giselarotatori@gmail.com>
2017-09-04 21:04:18 -0300

    Solucionado ISSUE #2 ahora actualiza el cifrado al mover la barra de desplazamiento

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
-document.getElementById('texto').addEventListener('keyup', function() {
+const desplazamiento = document.getElementById("desplazamiento");
+const texto = document.getElementById("texto");
+const textoCifrado = document.getElementById("cifrado");
+
+function cifrado(){
+	const textoIngresado = texto.value;
+	textoCifrado.value = textoIngresado.split('').map(c=>{
+		let valorEntero = c.charCodeAt(0);
+		if(valorEntero >= 97 && valorEntero <= 122)
+			{
+				const valorDesplazamiento = parseInt(desplazamiento.value);
+				
+				valorEntero = valorEntero + valorDesplazamiento;
+				if(valorEntero > 122)
+					valorEntero = 97 + valorEntero - 122 - valorDesplazamiento;
+			}
+	
+			return String.fromCharCode(valorEntero);
+		}).join('');
+}
+
+texto.addEventListener("keyup",cifrado);
+desplazamiento.addEventListener("change", cifrado);
+
+//FUNCION ORIGINAL
+/*document.getElementById('texto').addEventListener('keyup', function() {
 	const textoIgresado = this.value;
 
 	document.getElementById('cifrado').value = textoIgresado.split('').map(c => {
@@ -6,14 +31,14 @@ document.getElementById('texto').addEventListener('keyup', function() {
 
 		if(valorEntero >= 97 && valorEntero <= 122)
 		{
-			const valorDesplazamiento = parseInt(document.getElementById('desplazamiento').value);
+			const valorDesplazamiento = parseInt(desplazamiento.value);
 			
 			valorEntero = valorEntero + valorDesplazamiento;
-
 			if(valorEntero > 122)
 				valorEntero = 97 + valorEntero - 122 - valorDesplazamiento;
 		}
 
 		return String.fromCharCode(valorEntero);
 	}).join('');
-});
+});*/
+

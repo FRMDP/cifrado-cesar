3cce323193eefefc9bde3da1499eed5dfb962935
grotatori <giselarotatori@gmail.com>
2017-09-07 19:33:00 -0300

    Borrada la funcion original comentada

--- js/app.js
+++ js/app.js
@@ -21,24 +21,3 @@ function cifrado(){
 
 texto.addEventListener("keyup",cifrado);
 desplazamiento.addEventListener("change", cifrado);
-
-//FUNCION ORIGINAL
-/*document.getElementById('texto').addEventListener('keyup', function() {
-	const textoIgresado = this.value;
-
-	document.getElementById('cifrado').value = textoIgresado.split('').map(c => {
-		let valorEntero = c.charCodeAt(0);
-
-		if(valorEntero >= 97 && valorEntero <= 122)
-		{
-			const valorDesplazamiento = parseInt(desplazamiento.value);
-			
-			valorEntero = valorEntero + valorDesplazamiento;
-			if(valorEntero > 122)
-				valorEntero = 97 + valorEntero - 122 - valorDesplazamiento;
-		}
-
-		return String.fromCharCode(valorEntero);
-	}).join('');
-});*/
-
